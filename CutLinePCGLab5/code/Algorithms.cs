3e6a2d0607b730b18b9f10ce06575173a1912750
VorohobkoMaryia <mvv2201@rambler.ru>
2022-12-17 02:10:22 +0300

    1

---
+++ CutLinePCGLab5/code/Algorithms.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace PKG_5
+{
+    struct Vector
+    {
+        public Vector(KeyValuePair<PointF, PointF> a)
+        {
+            A = a.Key.X - a.Value.X;
+            B = a.Key.Y - a.Value.Y;
+        }
+        public Vector(float Vx, float Vy)
+        {
+            this.A = Vx;
+            this.B = Vy;
+        }
+        public Vector(PointF begin, PointF end)
+        {
+            A = begin.X - end.X;
+            B = begin.Y - end.Y;
+        }
+        public float A;
+        public float B;
+    }
+    struct forSutherlandCohen
+    {
+        public forSutherlandCohen(Point min, Point max)
+        {
+            minPoint = min;
+            pMax = max;
+        }
+        public forSutherlandCohen(float x_min, float y_min, float x_max, float y_max)
+        {
+            minPoint = new PointF(x_min, y_min);
+            pMax = new PointF(x_max, y_max);
+        }
+        public PointF minPoint;
+        public PointF pMax;
+    }
+    class Algorithms
+    {
+        public void SetRectangleClipper(float x_min, float y_min, float x_max, float y_max)
+        {
+            rect = new forSutherlandCohen(x_min, y_min, x_max, y_max);
+        }
+        public void SetRectangleClipper(Point min, Point max)
+        {
+            rect = new forSutherlandCohen(min, max);
+        }
+        
+        public string getCode(PointF a)
+        {
+            string code = "";
+            code += (a.Y > rect.pMax.Y ? '1' : '0');
+            code += (a.Y < rect.minPoint.Y ? '1' : '0');
+            code += (a.X > rect.pMax.X ? '1' : '0');
+            code += (a.X < rect.minPoint.X ? '1' : '0');
+            return code;
+        }
+        public PointF moveBit(PointF a, float k)
+        {
+            var code = getCode(a);
+            if (code[2] == '1')
+            {
+                return new PointF(rect.pMax.X, a.Y + k * (rect.pMax.X - a.X));
+            }
+            if (code[3] == '1')
+            {
+                return new PointF(rect.minPoint.X, a.Y + k * (rect.minPoint.X - a.X));
+            }
+            if (code[0] == '1')
+            {
+                return new PointF(a.X + (1 / k) * (rect.pMax.Y - a.Y), rect.pMax.Y);
+            }
+            if (code[1] == '1')
+            {
+                return new PointF(a.X + (1 / k) * (rect.minPoint.Y - a.Y), rect.minPoint.Y);
+            }
+            return a;
+        }
+        public bool SutherlandCohen(PointF a, PointF b, ref PointF p1, ref PointF p2)
+        {
+            p1 = a;
+            p2 = b;
+            var a_code = getCode(a);
+            var b_code = getCode(b);
+            if (a_code.Equals("0000") && b_code.Equals("0000")) {
+                p1 = new PointF(a.X, a.Y);
+                p2 = new PointF(b.X, b.Y);
+                return true;
+            }
+
+            var a_num = Convert.ToInt32(a_code, 2);
+            var b_num = Convert.ToInt32(b_code, 2);
+            if ((a_num & b_num) != 0)
+            {
+                p1 = new PointF(a.X, a.Y);
+                p2 = new PointF(b.X, b.Y);
+                return false;
+            }
+            float k = (b.Y - a.Y) / (b.X - a.X);
+            if (k == float.NaN)
+            {
+                k = float.PositiveInfinity;
+            }
+            if (a_num == 0)
+            {
+                Swap(ref a, ref b);
+                Swap(ref p1, ref p2);
+                a_code = getCode(a); 
+                b_code = getCode(b);
+                a_num = Convert.ToInt32(a_code, 2);
+                b_num = Convert.ToInt32(b_code, 2);
+            }
+            if (a_num != 0 && b_num == 0)
+            {
+                PointF p = moveBit(a, k);
+                while (!getCode(p).Equals("0000"))
+                {
+                    p = moveBit(p, k);
+                }
+                p1 = new PointF(p.X, p.Y);
+                p2 = new PointF(b.X, b.Y);
+                return true;
+            }
+                
+                float x, y;
+                List<PointF> points = new List<PointF>();
+            for (int i = 0; i < 2; i++)
+            {
+                x = p1.X + (1 / k) * (rect.pMax.Y - p1.Y);
+                if (x >= rect.minPoint.X && x <= rect.pMax.X)
+                {
+                    points.Add(new PointF(x, rect.pMax.Y));
+                }
+                if (points.Count == 2)
+                {
+                    p1 = points[0];
+                    p2 = points[1];
+                    return true;
+                }
+                x = p1.X + (1 / k) * (rect.minPoint.Y - p1.Y);
+                if (x >= rect.minPoint.X && x <= rect.pMax.X)
+                {
+                    points.Add(new PointF(x, rect.minPoint.Y));
+                }
+                if (points.Count == 2)
+                {
+                    p1 = points[0];
+                    p2 = points[1];
+                    return true;
+                }
+                y = p1.Y + k * (rect.pMax.X - p1.X);
+                if (y >= rect.minPoint.Y && y <= rect.pMax.Y)
+                {
+                    points.Add(new PointF(rect.pMax.X, y));
+                }
+                if (points.Count == 2)
+                {
+                    p1 = points[0];
+                    p2 = points[1];
+                    return true;
+                }
+                y = p1.Y + k * (rect.minPoint.X - p1.X);
+                if (y >= rect.minPoint.Y && y <= rect.pMax.Y)
+                {
+                    points.Add(new PointF(rect.minPoint.X, y));
+                }
+                if (points.Count == 2)
+                {
+                    p1 = points[0];
+                    p2 = points[1];
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void Swap(ref PointF a, ref PointF b)
+        {
+            PointF temp = a;
+            a = b;
+            b = temp;
+        }
+        public forSutherlandCohen rect;
+        public List<Vector> vectors;
+        public List<KeyValuePair<PointF, PointF>> edges;
+        public float ScalarMultiply(Vector vec1, Vector vec2)
+        {
+            return (vec1.A * vec2.A + vec1.B * vec2.B);
+        }
+
+        public float VectorMultiply(Vector vec1, Vector vec2)
+        {
+            return vec1.A * vec2.B - vec1.B * vec2.A;
+        }
+        public void SetPolygon(List<KeyValuePair<PointF, PointF>> list)
+        {
+            vectors = new List<Vector>();
+            edges = new List<KeyValuePair<PointF, PointF>>(list);
+            for (int i = 0; i < list.Count; i++)
+            {
+                vectors.Add(new Vector(list[i].Key, list[i].Value));
+            }
+
+        }
+
+        
+        public float getParameter(PointF p, KeyValuePair<PointF, PointF> segment)
+        {
+            return (p.X - segment.Key.X) / (segment.Value.X - segment.Key.X);
+        }
+        public float getT(KeyValuePair<PointF, PointF> sides, KeyValuePair<PointF, PointF> segment, ref bool onSameLine)
+        {
+            var x0e = sides.Key.X;
+            var y0e = sides.Key.Y;
+            var x1e = sides.Value.X;
+            var y1e = sides.Value.Y;
+
+
+            var x0s = segment.Key.X;
+            var y0s = segment.Key.Y;
+            var x1s = segment.Value.X;
+            var y1s = segment.Value.Y;
+
+            float ks = (y1s - y0s)/(x1s - x0s);
+            float ke = (y1e - y0e) / (x1e - x0e);
+
+            float bs = y0s - ks * x0s;
+            float be = y0e - ke * x0e;
+
+            var x = (be - bs) / (ks - ke);
+            if ((x - x0e) / (x1e - x0e) <= 0 || (x - x0e) / (x1e - x0e) >= 1)
+            {
+                return -1;
+            }
+            var te = (x - x0s) / (x1s - x0s);
+            if (float.IsNaN(te) && ke == ks && be == bs)
+            {
+                onSameLine = true;
+            }
+            return te;
+        }
+        public void CyrusBeck(PointF a, PointF b, ref float t_1, ref float t_2)
+        {
+            List<float> inT = new List<float>();
+            List<float> outT = new List<float>();
+            KeyValuePair<PointF, PointF> segment = new KeyValuePair<PointF, PointF>(a, b);
+            var vec_segment = new Vector(segment);
+            float t;
+            float S;
+            bool onSameLine = false;
+            for (int i = 0; i < edges.Count; i++)
+            {
+                t = getT(edges[i], segment, ref onSameLine);
+                if (onSameLine) {
+                    inT.Add(getParameter(edges[i].Key, segment));
+                    outT.Add(getParameter(edges[i].Value, segment));
+                    outT.Add(getParameter(edges[i].Key, segment));
+                    inT.Add(getParameter(edges[i].Value, segment));
+                    onSameLine = false;
+                    continue;
+                }
+                S = -ScalarMultiply(new Vector(-vectors[i].B, vectors[i].A), vec_segment);
+                if (t >=0 && t <= 1)
+                {
+                    if (S > 0)
+                    {
+                        inT.Add(t);
+                    }
+                    else if (S < 0)
+                    {
+                        outT.Add(t);
+                    }
+                    else
+                    {
+                        inT.Add(t);
+                        outT.Add(t);
+                    }
+                }
+            }
+            if (outT.Count == 0 && inT.Count == 0)
+            {
+                t_1 = -1;
+                t_2 = -1;
+                return;
+            }
+            float tin = 0;
+            float tout = 1;
+            for (int i = 0; i < inT.Count; i++)
+            {
+                if (tin < inT[i])
+                {
+                    tin = inT[i];
+                }
+            }
+
+            for (int i = 0; i < outT.Count; i++)
+            {
+                if (tout > outT[i])
+                {
+                    tout = outT[i];
+                }
+            }
+            t_1 = tin;
+            t_2 = tout;
+        }
+    }
+}
---
+++ CutLinePCGLab5/code/Application.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace PKG_5
+{
+    partial class Application
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 535);
+            this.panel1.TabIndex = 0;
+            this.panel1.SizeChanged += new System.EventHandler(this.panel1_SizeChanged);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(877, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Изменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(801, 397);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(70, 27);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            14,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(803, 371);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Масштаб (в пикселях)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(800, 461);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(181, 80);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Алгоритмы отсечения";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(117, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Кируса-Бека";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(160, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Сазерленда-Коэна";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(220, 380);
+            this.Name = "Form1";
+            this.Text = "Приложение";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
